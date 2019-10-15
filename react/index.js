d17d5951d7fadda78b3850f50789d16ebabc9f15
guoyiheng <mypridelife@163.com>
2019-10-15 10:58:46 +0800

    react todos

--- react/index.js
+++
@@ -1,7 +0,0 @@
-// import { createStore } from 'redux'
-// import todoApp from './reducers'
-
-// let store = createStore(todoApp)
-
-const Todo = ({ onClick, completed, text }) => console.log()
-console.log(Todo.propTypes)
