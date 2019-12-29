e8615d64543ab65126b3f315e8aaacd22aa4cec4
PC-gyh <mypridelife@163.com>
2019-12-29 17:02:13 +0800

    add: mkv

--- project/handleDownLoadFilm/isFilm.js
+++ project/handleDownLoadFilm/isFilm.js
@@ -3,6 +3,6 @@
  * @param {string} name 文件名
  */
 function isFilm(name) {
-    return name.endsWith(".mp4") || name.endsWith(".wmv") || name.endsWith(".rmvb") || name.endsWith(".avi") || name.endsWith(".rm") || name.endsWith(".mov") || name.endsWith(".flv")
+    return name.endsWith(".mp4") || name.endsWith(".wmv") || name.endsWith(".rmvb") || name.endsWith(".avi") || name.endsWith(".rm") || name.endsWith(".mov") || name.endsWith(".flv") || name.endsWith(".mkv")
 }
 module.exports = isFilm
