80c183f9083f0d17964efeaf0961b3d276b1d825
guoyiheng <mypridelife@163.com>
2020-05-22 14:07:58 +0800

    。

--- js/es/array-string.js
+++ js/es/array-string.js
@@ -7,3 +7,9 @@ Array.prototype.multiply = function () {
 }
 a.multiply()
 console.log(a)
+
+const uri = 'https://share.mypetcantalk.com/class/?from=singlemessage&isappinstalled=0#/groupdata?groupid=38ffe7d0c13611e8888d233421222e5a&VNK=b77d2cd5&id=bc79f5509a4d11ea9be2bad6cb08b09d'
+console.log(uri.split('#'))
+console.log(uri.split('#')[0])
+console.log(uri.split('#')[1])
+console.log(uri.split('#')[0] + '#/groupdata?groupid=' + 'hhh' + '&id=' + 'hhh')
