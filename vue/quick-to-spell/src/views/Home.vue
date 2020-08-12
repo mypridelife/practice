f98c882c4054b4c5b1284094c901ffcbe6a9013f
guoyiheng <mypridelife@163.com>
2020-08-12 18:35:00 +0800

    .

--- vue/quick-to-spell/src/views/Home.vue
+++ vue/quick-to-spell/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="m-hello">hello world</div>
+    <hello-world />
+    <div class="element">hello world3</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { HelloWorld }
 }
 </script>
+
+<style scoped lang="scss" type="text/css">
+.m-hello {
+  font-size: 32px;
+}
+.element {
+  width: 400px;
+  height: 400px;
+  /* rebasing path by new destination */
+  background: url('../assets/logo.png');
+}
+</style>
