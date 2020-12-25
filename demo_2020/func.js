2a3575f8de7897cd9bdab9e4fcf9811bc5c214c3
gyh <mypridelife@163.com>
2020-12-25 17:44:49 +0800

    .

--- demo_2020/func.js
+++ demo_2020/func.js
@@ -1,26 +1,2 @@
-const fetch = require("node-fetch")
-
-// fetch("https://weread.qq.com/web/book/read", {
-//   headers: {
-//     accept: "application/json, text/plain, */*",
-//     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-//     "content-type": "application/json;charset=UTF-8",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-origin",
-//     cookie:
-//       "pgv_pvi=590593024; pgv_si=s71208960; ptui_loginuin=342166082; RK=3XwdwHjTR+; ptcz=1eeada14b4051316bfd2e82cf01c09f6b5619d930b8f34794f2023658dc2648e; pgv_pvid=3324601504; eas_sid=V1f5C9J7n8n2r2A3s4k6o9T1E3; ied_qq=o0342166082; eafifaqqcomrouteLine=main_main; gameqqcomrouteLine=index; o_cookie=342166082; uin_cookie=o0342166082; rv2=8078D18BCC47B718A5B4A40BF584D89FA51697A5531E5A36BF; property20=45675F02C59EA39DF54C3921880205864C3B68589F5F8B284710DACF782AF5AFEF438B56B71D97F3; sd_userid=75861600742069066; sd_cookie_crttime=1600742069066; lolqqcomrouteLine=index-tool_main_index-tool_index-page_index-tool_main_index-tool; tvfe_boss_uuid=84b2b46c470919fe; lplqqcomrouteLine=schedule_a20200324lpl_a20200324lpl_a20200801message_a20200801message_a20200901worlds_detail; LW_uid=V1a6a0J164x395S0R865O9W5t6; lolmqqcomrouteLine=index_index_news-detail; tokenParams=%3Fe_code%3D507042; LW_sid=j1g6q0W4s4n725z0n3O8n0A3G6; uin=null; skey=null; luin=null; lskey=null; user_id=null; session_id=null; pgv_info=ssid=s6383156239&pgvReferrer=; wr_gid=268823824; wr_theme=dark; wr_vid=273754307; wr_pf=0; wr_rt=web%40A8CC1v6gwpM4SloFIOC_WL; wr_localvid=9d4325c08105128c39d4a4a; wr_name=shall%20we%20talk; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FibD5qS2OcJj6q0UD0e2vbGqvDicRv4TY92DrKZtoC3poicXSnBibEI1ibHWtjSGFW0D1Y7iaBqP6Q2nLq7r7nJVITibkw%2F132; wr_gender=1; pac_uid=1_ull; wr_skey=U3Rjh1Sn",
-//   },
-//   referrer: "https://weread.qq.com/web/reader/fa3322105ca0ecfa382cac6ke3632bd0222e369853df322",
-//   referrerPolicy: "strict-origin-when-cross-origin",
-//   body:
-//     '{"appId":"wb115321887460h620914690","b":"fa3322105ca0ecfa382cac6","c":"e3632bd0222e369853df322","ci":34,"co":0,"sm":"他不知道谁和谁为什么打起架来了。骂声、打","pr":0,"rt":3,"ts":1605082813511,"rn":351,"sg":"53f2299e838faf3310341473a61798cb3545054f87fc628e65f76d17f4f8ab5c","ct":1605082813,"ps":"9673275079912926g0195ca","pc":"e88320f079912927g0145b9","s":"1663d249"}',
-//   method: "POST",
-//   mode: "cors",
-// })
-// 1605082731391
-// 1605082813511
-// 1605082877851
-// 1605082035470
-
-// console.log((1605084886387 - 1605084856343) / 1000 / 60)
+var matches = /(hello \S+)/.exec('This is a hello world!')
+console.log(matches)
--- demo_2020/index.js
+++ demo_2020/index.js
@@ -1,38 +1,10 @@
-// var express = require('express')
-// var fs = require('fs')
-// var app = express()
-
-// //方法1：通过express.static访问静态文件，这里访问的是ajax.html
-// // app.use(express.static("./"));
-
-// //方法2：使用fs.readFile打开html文件
-// app.get('./index.html', function(request, response) {
-//   fs.readFile('./' + request.path.substr(1), function(err, data) {
-//     // body
-//     if (err) {
-//       console.log(err)
-//       //404：NOT FOUND
-//       response.writeHead(404, { 'Content-Type': 'text/html' })
-//     } else {
-//       //200：OK
-//       response.writeHead(200, { 'Content-Type': 'text/html' })
-//       response.write(data.toString())
-//     }
-//     response.end()
-//   })
-// })
-
-// app.listen(3000, function() {
-//   //监听http://127.0.0.1:3000端口
-//   console.log('server start')
-// })
 var http = require('http') //创建服务器的
 var fs = require('fs')
 //引入进来的是模块，模块中有方法，下一步就是使用方法
 //Node.js一个最主要的特点：执行的基本都是函数
 
 //创建服务
-var myServer = http.createServer(function(req, res) {
+var myServer = http.createServer(function (req, res) {
   //req->请求变量：客户端请求服务器的
   //res->响应变量:服务器要给客户端写回的变量
   //前端页面应该给客户端显示，即写回去
@@ -48,7 +20,7 @@ var myServer = http.createServer(function(req, res) {
 
 //服务端等着客户端请求需要做一个监听。通过创建的服务。
 //监听
-myServer.listen('8000', function(err) {
+myServer.listen('8000', function (err) {
   if (err) {
     console.log(err)
     throw err
