33db712971c28d39babd4d8c1742afb89cdf8234
gyh <mypridelife@163.com>
2020-12-29 14:22:23 +0800

    。

--- demo_2020/func.js
+++ demo_2020/func.js
@@ -1,2 +1,11 @@
 var matches = /(hello \S+)/.exec('This is a hello world!')
 console.log(matches)
+
+function add(a, b) {
+  return a + b
+}
+var i = 1,
+  j = 2
+var c = add(i, j)
+
+console.log(c, i, j)
