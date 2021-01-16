de7b21d6e59b1ede52e6f1969e73f360247c1e2b
gyh <mypridelife@163.com>
2021-01-16 08:07:20 +0800

    .

--- demo_2020/classFunc.js
+++ demo_2020/classFunc.js
@@ -1,33 +1,39 @@
-class User {
-  name = 'John'
-  age = '18'
-  sayName() {
-    console.log(this.name)
-  }
-  sayAge = function () {
-    console.log(this.age)
-  }
+// class User {
+//   name = 'John'
+//   age = '18'
+//   sayName() {
+//     console.log(this.name)
+//   }
+//   sayAge = function () {
+//     console.log(this.age)
+//   }
+// }
+// let user = new User()
+// console.log(user.name) // John
+// console.log(User.prototype.name) // undefined
+// console.log(user.sayName) // [Function: sayName]
+// console.log(User.prototype.sayName) // [Function: sayName]
+// console.log(user.sayAge) //[Function: sayAge]
+
+// console.log(User.prototype.sayAge) // undefined
+
+// let getSingle = function (fn) {
+//   let result
+//   console.log('arguments', arguments)
+
+//   return (...args) => {
+//     return result || (result = fn.apply(this, args))
+//   }
+// }
+// let demo = function (val) {
+//   console.log('val', val)
+// }
+
+// let single = getSingle(demo, 'hhh')
+// single()
+
+function formatZero(num, len) {
+  if (String(num).length > len) return num
+  return (Array(len).join(0) + num).slice(-len)
 }
-let user = new User()
-console.log(user.name) // John
-console.log(User.prototype.name) // undefined
-console.log(user.sayName) // [Function: sayName]
-console.log(User.prototype.sayName) // [Function: sayName]
-console.log(user.sayAge) //[Function: sayAge]
-
-console.log(User.prototype.sayAge) // undefined
-
-let getSingle = function (fn) {
-  let result
-  console.log('arguments', arguments)
-
-  return (...args) => {
-    return result || (result = fn.apply(this, args))
-  }
-}
-let demo = function (val) {
-  console.log('val', val)
-}
-
-let single = getSingle(demo, 'hhh')
-single()
+console.log(formatZero([1, 2], 4))
